26c15ab7c124e089e0a10bbde58996dae2b56f72
Isaac Iguelmamene <32892275+iiguelmamene@users.noreply.github.com>
2018-08-26 13:17:45 -0700

    Update redditreview.py

--- redditreview.py
+++ redditreview.py
@@ -4,6 +4,7 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.feature import CountVectorizerModel
 
